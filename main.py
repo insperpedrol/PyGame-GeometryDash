fe4297f2e2d6b33feb3562cee4b7e878a327af6f
insperpedrol <pedrobscl@al.insper.edu.br>
2024-11-06 09:05:42 -0300

    v5

--- main.py
+++ main.py
@@ -5,25 +5,37 @@ import random
 pygame.init()
 
 # ----- Gera tela principal
-WIDTH = 1080
+WIDTH = 720
 HEIGHT = 720
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Geometry Dash - Python Edition')
 # ----- Inicia assets
-CUBO_WIDTH = 45
-CUBO_HEIGHT = 45
+player_WIDTH = 75
+player_HEIGHT = 75
 
 sprites = {}
-sprites['background'] = pygame.image.load("assets\\img\\fundo.png").convert()
+sprites['background'] = pygame.image.load("assets\\img\\background.png").convert()
 sprites['Player'] = pygame.image.load('assets\img\cubo.png').convert_alpha()
 
-sprites['Player'] = pygame.transform.scale(sprites['Player'], (CUBO_WIDTH, CUBO_HEIGHT))
+sprites['Player'] = pygame.transform.scale(sprites['Player'], (player_WIDTH, player_HEIGHT))
+
+
+backcolorWIDTH = 720
+backcolorHEIGHT = backcolorWIDTH
+backcolor = pygame.Surface((backcolorWIDTH, backcolorHEIGHT))
+backcolor.set_alpha(128)
+backcolor.fill((53, 107, 232))
+
+backcolorWIDTH2 = 720
+backcolorHEIGHT2 = backcolorWIDTH2
+backcolor2 = pygame.Surface((backcolorWIDTH2, backcolorHEIGHT2))
+backcolor2.set_alpha(128)
+backcolor2.fill((52, 128, 235))
 
 # ----- Inicia estruturas de dados
 
 game = True
 clock = pygame.time.Clock()
-
 
 class Player(pygame.sprite.Sprite):
         def __init__(self,img):
@@ -31,12 +43,11 @@ class Player(pygame.sprite.Sprite):
             self.image = img
 
             self.rect = self.image.get_rect()
-            self.rect.centerx = WIDTH / 3
+            self.rect.centerx = WIDTH / 5.2
             self.rect.bottom = HEIGHT / 1.3
 
-            self.speedx = 1.5
-            self.gravity = 0.25
-            self.jump_speed = -7
+            self.gravity = 0.3
+            self.jump_speed = -9
             self.speedy = 2.5
             self.on_ground = True
 
@@ -46,8 +57,6 @@ class Player(pygame.sprite.Sprite):
                 self.on_ground = False
 
         def update(self):
-            self.rect.x += self.speedx
-
             if not self.on_ground:
                 self.speedy += self.gravity
                 self.rect.y += self.speedy
@@ -55,6 +64,18 @@ class Player(pygame.sprite.Sprite):
                     self.rect.bottom = HEIGHT / 1.3
                     self.on_ground = True
                     self.speedy = 0
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, img):
+        self.img = img
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, img):
+        self.img = img
+
+class Spike(pygame.sprite.Sprite):
+    def __init__(self, img):
+        self.img = img
 
 all_sprites = pygame.sprite.Group()
 player = Player(sprites['Player'])
@@ -74,7 +95,14 @@ while game:
 
     # ----- Gera saídas
     window.fill((255, 255, 255))  # Preenche com a cor branca
-    window.blit(sprites['background'], (0, -180))
+
+    # Coloca o background
+    window.blit(sprites['background'], (0, 0))
+    window.blit(backcolor, (0, 0))
+
+    # Coloca o chao
+    window.blit(sprites['background'], (0, HEIGHT / 1.3))
+    window.blit(backcolor2, (0, HEIGHT / 1.3))
     all_sprites.draw(window)
 
     # ----- Atualiza estado do jogo
